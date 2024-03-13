d45313ac2f31a5b124d0ea31ed56cd07293f9e92
Hashi1995 <s223495239@deakin.edu.au>
2024-03-13 15:07:11 +1100

    add files

---
+++ script.js
@@ -0,0 +1,10 @@
+function changeText() {
+    let potentialTexts = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+    let randomNumber = getRandomNumber(0, potentialTexts.length -1);
+    let textToChangeTo = potentialTexts[randomNumber];
+    document.getElementById('header').innerHTML = textToChangeTo;
+}
+
+function getRandomNumber(min,max){
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
